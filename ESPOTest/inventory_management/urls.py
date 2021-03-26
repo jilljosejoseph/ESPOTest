72a56005b93489959f2037cb6d92a197f395df60
Jill Joseph <JJJoseph@my.staugustine.edu>
2021-03-25 20:22:14 -0500

    first commit not tested

--- ESPOTest/inventory_management/urls.py
+++ ESPOTest/inventory_management/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("item/add", views.add_item),
+    path("items/<int:item_id>", views.item_display),
+    path("items/<int:item_id>/delete", views.delete_item),
 ]
--- ESPOTest/inventory_management/views.py
+++ ESPOTest/inventory_management/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Inventory
 
 def home(request):
-    # Your code here!
-    return render(request, "home.html")
+    context = {
+        'all_items': Inventory.objects.all()
+    }
+    return render(request, "home.html", context)
 
+def add_item(request):
+    Inventory.objects.create(
+        item_name = request.POST['item'], 
+        quantity = request.POST['quantity'], 
+        sell_price = request.POST['sell'], 
+        buy_price = request.POST['buy'])
+    return redirect('/')
+
+def item_display(request, item_id):
+    context = {
+        'item' : Inventory.objects.get(id = item_id)
+    }
+    return render(request, 'item.html', context)
+
+def delete_item(request, item_id):
+    item = Inventory.objects.get(id = item_id)
+    item.delete()
+    return redirect('/')
 # Feel free to make more views as needed
